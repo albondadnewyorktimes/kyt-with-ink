60c053d4311e07ac16417fc4b6daf44df3103bfd
albondadnewyorktimes <albondadnewyorktimes>
2023-01-18 10:11:16 -0600

    Update kyt config to support ink components

--- kyt.config.js
+++ kyt.config.js
@@ -1,6 +1,29 @@
-// Base kyt config.
-// Edit these properties to make changes.
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
+const modfiyWebpackConfigForNytCmsInk = (config, options) => {
+  const babelLoaderRule = config.module.rules.find((element) => element.loader === 'babel-loader');
+  babelLoaderRule.exclude = undefined;
+  babelLoaderRule.include = [
+    path.resolve(__dirname, 'node_modules/@nyt-cms/ink'),
+    path.resolve(__dirname, 'src'),
+  ];
+
+  if (options.type === 'server') {
+    config.externals = [
+      nodeExternals({
+        modulesDir: options.modulesDir,
+        allowlist: ['webpack/hot/poll?300', '@nyt-cms/ink'],
+      }),
+    ];
+  }
+
+  return config;
+};
 module.exports = {
   debug: false,
+  modifyWebpackConfig: (config, options) => {
+    config = modfiyWebpackConfigForNytCmsInk(config, options);
+    return config;
+  },
 };
